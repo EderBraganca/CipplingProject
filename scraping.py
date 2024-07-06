c9504ecca5bd6dd3420a2ca69c5003fb1c4f8a20
Eder Bragança <88894912+EderBraganca@users.noreply.github.com>
2024-07-06 12:18:46 -0300

    Versão final

--- scraping.py
+++ scraping.py
@@ -39,6 +39,9 @@ def scrape_news_estado_de_minas(url):
     return news_data
 
 def scrape_news_o_tempo(url):
+    position = url.find('.br') + 3  # +3 to include '.br'
+    cropped_url = url[:position]
+
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     news_data = []
@@ -46,20 +49,12 @@ def scrape_news_o_tempo(url):
     for item in soup.find_all('h2', class_='list__description'):
         title = item.get_text(strip=True) #Texto sem espaços em branco
         parent = item.find_parent('a') #Como eu utilizei o h2, eu preciso voltar um nível para pegar o link
-        href = parent['href'] if parent and parent.has_attr('href') else ''
+        href = parent['href'] 
+        if href and href.startswith('/'):
+            href = cropped_url + href
         news_data.append((title, href))
 
     return news_data
-
-def gather_news_itatiaia(all_news, k):
-    politica_url = 'https://www.itatiaia.com.br/politica'  
-    economia_url = 'https://www.itatiaia.com.br/economia'  
-    
-    politica_news = scrape_news_itatiaia(politica_url)[:k]
-    economia_news = scrape_news_itatiaia(economia_url)[:k]
-
-    all_news['política'] = politica_news
-    all_news['economia'] = economia_news
 
 def gather_news_EM(news_sources):
     all_news = {}
@@ -70,6 +65,16 @@ def gather_news_EM(news_sources):
         all_news[category].extend(news)
 
     return all_news
+
+def gather_news_itatiaia(all_news, k):
+    politica_url = 'https://www.itatiaia.com.br/politica'  
+    economia_url = 'https://www.itatiaia.com.br/economia'  
+    
+    politica_news = scrape_news_itatiaia(politica_url)[:k]
+    economia_news = scrape_news_itatiaia(economia_url)[:k]
+
+    all_news['política'] = politica_news
+    all_news['economia'] = economia_news
 
 def gather_news_o_tempo(all_news, k):
     politica_url = 'https://www.otempo.com.br/politica'  
@@ -94,9 +99,12 @@ def format_news(all_news):
     return news_text
 
 if __name__ == "__main__":
+    #Não vi necessidade de importar essas bibliotecas, entao comentei
     #from datetime import datetime
-    #from tqdm import tqdm
+    #from tqdm import tqdm 
 
+    #O número de noticias que o usuário só funcionam para o Itatiaia e o O Tempo,
+    #pois o código do EM já foi previamente fornecido e eu não quis mexer nele
     k = int(input("Quantas notícias deseja ver de cada tópico deseja ver? "))
 
     # News websites and categories for Estado de Minas
@@ -104,30 +112,29 @@ if __name__ == "__main__":
         'https://www.em.com.br/politica/': 'Política',
         'https://www.em.com.br/economia/': 'Economia',
         'https://www.em.com.br/educacao/': 'Educação',
-    }
+    }   
 
-    # Gather news from Estado de Minas
+    #------------------------Recebimento de notícias de Estado de Minas 
     all_news_EM = gather_news_EM(news_sources_EM)
-    print (all_news_EM)
     news_text_EM = format_news(all_news_EM)
+
     print("Estado de Minas News:")
     print(news_text_EM)
 
-    #-----------------------------------------------
+    #------------------------Recebimento de notícias de Itatiaia 
     all_news_itatiaia = {}
 
     gather_news_itatiaia(all_news_itatiaia, k)
-    print(all_news_itatiaia)
     news_text_itatiaia = format_news(all_news_itatiaia)
 
     print("Itatiaia News:")
     print(news_text_itatiaia)
 
-    #-----------------------------------------------
+    #------------------------Recebimento de notícias de O Tempo
     all_news_o_tempo = {}
 
     gather_news_o_tempo(all_news_o_tempo, k)
     news_text_o_tempo = format_news(all_news_o_tempo)
-
+    
     print("O Tempo News:")
     print(news_text_o_tempo)
